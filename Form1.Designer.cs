61796bb6fcfd5cd540cac26fef8f9460203a6630
Laura Naville <lauranaville1@gmail.com>
2021-05-22 14:31:44 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿
+namespace JogoDaVelha
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Oponto = new System.Windows.Forms.Label();
+            this.empate = new System.Windows.Forms.Label();
+            this.Xponto = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnJogar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::JogoDaVelha.Properties.Resources.WhatsApp_Image_2021_05_14_at_14_30_06_removebg_preview;
+            this.pictureBox4.Location = new System.Drawing.Point(206, -34);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(339, 180);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btn);
+            this.panel1.Controls.Add(this.btn8);
+            this.panel1.Controls.Add(this.btn9);
+            this.panel1.Controls.Add(this.btn7);
+            this.panel1.Controls.Add(this.btn5);
+            this.panel1.Controls.Add(this.btn6);
+            this.panel1.Controls.Add(this.btn4);
+            this.panel1.Controls.Add(this.btn2);
+            this.panel1.Controls.Add(this.btn3);
+            this.panel1.Location = new System.Drawing.Point(-2, 92);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(747, 271);
+            this.panel1.TabIndex = 23;
+            // 
+            // btn
+            // 
+            this.btn.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn.Location = new System.Drawing.Point(226, 19);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(99, 80);
+            this.btn.TabIndex = 0;
+            this.btn.UseVisualStyleBackColor = false;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn8.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn8.Location = new System.Drawing.Point(338, 191);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(99, 80);
+            this.btn8.TabIndex = 7;
+            this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn9.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn9.Location = new System.Drawing.Point(450, 191);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(99, 80);
+            this.btn9.TabIndex = 8;
+            this.btn9.UseVisualStyleBackColor = false;
+            this.btn9.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn7.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn7.Location = new System.Drawing.Point(226, 191);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(99, 80);
+            this.btn7.TabIndex = 6;
+            this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn5.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn5.Location = new System.Drawing.Point(338, 105);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(99, 80);
+            this.btn5.TabIndex = 4;
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn6.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn6.Location = new System.Drawing.Point(450, 105);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(99, 80);
+            this.btn6.TabIndex = 5;
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn4.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn4.Location = new System.Drawing.Point(226, 105);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(99, 80);
+            this.btn4.TabIndex = 3;
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn2.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn2.Location = new System.Drawing.Point(338, 19);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(99, 80);
+            this.btn2.TabIndex = 1;
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn3.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn3.Location = new System.Drawing.Point(449, 19);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(99, 80);
+            this.btn3.TabIndex = 2;
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.Oponto);
+            this.panel2.Controls.Add(this.empate);
+            this.panel2.Controls.Add(this.Xponto);
+            this.panel2.Controls.Add(this.pictureBox3);
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Location = new System.Drawing.Point(-2, 384);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(498, 171);
+            this.panel2.TabIndex = 24;
+            // 
+            // Oponto
+            // 
+            this.Oponto.AutoSize = true;
+            this.Oponto.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Oponto.Location = new System.Drawing.Point(208, 126);
+            this.Oponto.Name = "Oponto";
+            this.Oponto.Size = new System.Drawing.Size(32, 37);
+            this.Oponto.TabIndex = 28;
+            this.Oponto.Text = "0";
+            // 
+            // empate
+            // 
+            this.empate.AutoSize = true;
+            this.empate.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.empate.Location = new System.Drawing.Point(415, 80);
+            this.empate.Name = "empate";
+            this.empate.Size = new System.Drawing.Size(32, 37);
+            this.empate.TabIndex = 27;
+            this.empate.Text = "0";
+            // 
+            // Xponto
+            // 
+            this.Xponto.AutoSize = true;
+            this.Xponto.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Xponto.Location = new System.Drawing.Point(208, 49);
+            this.Xponto.Name = "Xponto";
+            this.Xponto.Size = new System.Drawing.Size(32, 37);
+            this.Xponto.TabIndex = 26;
+            this.Xponto.Text = "0";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::JogoDaVelha.Properties.Resources.WhatsApp_Image_2021_05_14_at_14_30_06__1__removebg_preview;
+            this.pictureBox3.Location = new System.Drawing.Point(42, 89);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(198, 125);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 25;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::JogoDaVelha.Properties.Resources.WhatsApp_Image_2021_05_14_at_14_30_06__3__removebg_preview;
+            this.pictureBox2.Location = new System.Drawing.Point(42, 10);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(198, 129);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 24;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::JogoDaVelha.Properties.Resources.WhatsApp_Image_2021_05_14_at_14_32_19_removebg_preview;
+            this.pictureBox1.Location = new System.Drawing.Point(266, 48);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(194, 123);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 23;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnJogar
+            // 
+            this.btnJogar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnJogar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnJogar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnJogar.Location = new System.Drawing.Point(562, 442);
+            this.btnJogar.Name = "btnJogar";
+            this.btnJogar.Size = new System.Drawing.Size(150, 50);
+            this.btnJogar.TabIndex = 29;
+            this.btnJogar.Text = "Jogar Novamente";
+            this.btnJogar.UseVisualStyleBackColor = false;
+            this.btnJogar.Click += new System.EventHandler(this.btnJogar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Honeydew;
+            this.ClientSize = new System.Drawing.Size(774, 561);
+            this.Controls.Add(this.btnJogar);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jogo da Velha";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnJogar;
+        private System.Windows.Forms.Label Oponto;
+        private System.Windows.Forms.Label empate;
+        private System.Windows.Forms.Label Xponto;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JogoDaVelha
+{
+    public partial class Form1 : Form
+    {
+
+        int Xplayer = 0, Oplayer = 0, empatesPontos = 0, rodadas = 0;
+        bool turno = true, jogo_final = false;
+        string[] texto = new string[9];
+
+        private void btnJogar_Click(object sender, EventArgs e)
+        {
+            btn.Text = "";
+            btn2.Text = "";
+            btn3.Text = "";
+            btn4.Text = "";
+            btn5.Text = "";
+            btn6.Text = "";
+            btn7.Text = "";
+            btn8.Text = "";
+            btn9.Text = "";
+
+            rodadas = 0;
+            jogo_final = false;
+            for (int i = 0; i < 9; i++) 
+            {
+                texto[i] = "";
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+           Button btn = (Button)sender;
+            int buttonIndex = btn.TabIndex;
+
+            if (btn.Text == "" && jogo_final == false)
+            {
+
+                if (turno)
+                {
+                    btn.Text = "X";
+                    texto[buttonIndex] = btn.Text;
+                    rodadas++;
+                    turno = !turno;
+                    Checagem(1);
+                }
+                else
+                {
+                    btn.Text = "O";
+                    texto[buttonIndex] = btn.Text;
+                    rodadas++;
+                    turno = !turno;
+                    Checagem(2);
+                }//FIM if
+
+            }
+        }// FIM metodo botao 
+
+        void Vencedor(int PlayerQueGanhou)
+        {
+            jogo_final = true;
+
+            if (PlayerQueGanhou == 1)
+            {
+                Xplayer++;
+                Xponto.Text = Convert.ToString(Xplayer);
+                MessageBox.Show("Jogador X Ganhou!");
+                turno = true;
+            }
+            else
+            {
+                Oplayer++;
+                Oponto.Text = Convert.ToString(Oplayer);
+                MessageBox.Show("Jogador O ganhou!");
+                turno = false;
+            }
+        }
+
+        void Checagem (int ChecagemPlayer)
+        {
+            string suporte = "";
+
+            if(ChecagemPlayer == 1)
+            {
+                suporte = "X";
+            }
+            else
+            {
+                suporte = "O";
+            }//FIM suporte
+
+            for (int horizontal = 0; horizontal < 8; horizontal += 3) //aumenta de 3 em 3
+            {
+               if (suporte == texto[horizontal]) 
+                { 
+                    if (texto[horizontal] == texto[horizontal + 1] && texto[horizontal] == texto[horizontal + 02])
+                    {
+                        Vencedor(ChecagemPlayer);
+                        return;
+                    }//FIM checagem horizontal
+
+                }
+
+            }//FIM for
+
+
+            //checagem vertical 
+
+            for (int vertical = 0; vertical < 3; vertical++) 
+            {
+                if (suporte == texto[vertical])
+                {
+                    if (texto[vertical] == texto[vertical + 3] && texto[vertical] == texto[vertical + 6])
+                    {
+                        Vencedor(ChecagemPlayer);
+                        return;
+                    }//FIM checagem verticsl
+
+                }
+
+            }//FIM for
+
+            //checagem diagonais
+
+            if (texto[0] == suporte)
+            {
+
+                if (texto[0] == texto[4] && texto[0] == texto[8])
+                {
+                    Vencedor(ChecagemPlayer);
+                    return;
+                }//FIM checagem diagonal principaç
+
+            }
+
+            if(texto[2] == suporte)
+            {
+                if (texto[2] == texto[4] && texto[2] == texto[6])
+                {
+                    Vencedor(ChecagemPlayer);
+                    return;
+                }//FIM checagem diagonal secundaria  
+            }
+
+            if (rodadas == 9 && jogo_final == false)
+            {
+                empatesPontos++;
+                empate.Text = Convert.ToString(empatesPontos);
+                MessageBox.Show("Empate!");
+                jogo_final = true;
+                return;
+            }
+            
+
+
+        }
+
+
+    }
+}
